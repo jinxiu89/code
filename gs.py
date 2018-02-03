318f390b68d13cc8bfa8391656f9bb87d8e318b5
jinxiu89 <jinxiu89@163.com>
2018-02-03 09:44:39 +0800

    jsqgz

---
+++ gs.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import csv
+class Args(object):
+    def __init__(self):
+        self.args=sys.argv[1:]
+    pass
+    ## todo::duqucnashu hanshu ,fanhui lujingdeng:
+class Config():
+    pass
